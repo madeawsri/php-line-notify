4ec0568651ebae21ba6e71ab7df09ff7b06facb2
wannasit <cmumad@gmail.com>
2022-02-15 12:44:10 +0700

    add comment

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include_once "config.class.php";
 include_once "line.class.php";
 
+// ลองเติม comment ตรงนี้ดูนะคับ
 $Line = new Line();
 $message = "Hello Notify!";
 echo $Line->send_notify($message);
